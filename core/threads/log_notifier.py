c1f5266cf820994956972a644f49124380cba5c3
h0nda <41300123+h0nde@users.noreply.github.com>
2022-02-14 12:57:01 +0100

    Update log_notifier.py

--- core/threads/log_notifier.py
+++ core/threads/log_notifier.py
@@ -4,10 +4,11 @@ def log_notifier(log_queue, webhook_url):
     while True:
         date, group_info = log_queue.get()
 
-        print(f"[{date.strftime('%H:%M:%S')}]",
-              f"roblox.com/groups/{group_info['id']:08d}",
-              "|", f"{str(group_info['memberCount']).rjust(2)} member(s)",
-              "|", group_info["name"])
+        print(
+            f"[{date.strftime('%H:%M:%S')}]",
+            f"roblox.com/groups/{group_info['id']:08d}",
+            "|", f"{str(group_info['memberCount']).rjust(2)} member{'s' if group_info['memberCount'] == 1 else ' '}",
+            "|", group_info["name"])
             
         if webhook_url:
             try:
